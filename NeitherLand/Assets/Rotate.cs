fee5302e27d9d5c8ebd9a05bf8433e966d95cfe7
Paulina Bugiel <nemesis7c5@gmail.com>
2015-08-24 22:08:51 +0200

    First commit

---
+++ NeitherLand/Assets/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    private float rotTimeStart, rotTimeLength;
+    int rotationLength = 100;
+    Vector3 rotStart;
+	Vector3 rotFinish;
+    
+    // Use this for initialization
+	void Start () {
+        rotTimeStart  = Time.time;
+        rotTimeLength = rotationLength * Time.deltaTime;
+        rotStart = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
+        rotFinish = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.Lerp(rotStart, rotFinish, (Time.time - rotTimeStart)/rotTimeLength));
+        if (Time.time >= rotTimeStart + rotTimeLength)
+        {
+            rotStart = rotFinish;
+            rotFinish = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
+            rotTimeStart = Time.time;
+        }
+	}
+}
